240e2e1d2b0086af7814ee434e3b829d311bbbf9
Anil Prajapati <anilprajapati.bs22@gmail.com>
2023-03-30 16:01:24 +0000

    sgnons

---
+++ frontend/copy_files.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+rm -r codex-liam-repo/frontend
+rm -r codex-liam-repo/backend
+scp -r presidio@192.168.1.11:/Users/presidio/sgn/metamorf/codex-code/frontend /home/sgnons/codex-liam-repo/
+scp -r presidio@192.168.1.11:/Users/presidio/sgn/metamorf/codex-code/backend /home/sgnons/codex-liam-repo/
+cd /home/sgnons/codex-liam-repo/
+git add .
+git commit -m $1
+git push origin sgnons-chart 
