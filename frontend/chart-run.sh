349400505f5993bc91c1a342624b9faf731505af
Anil Prajapati <anilprajapati.bs22@gmail.com>
2023-03-30 15:59:04 +0000

    sgnons

--- frontend/chart-run.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-sudo systemctl daemon-reload
-sudo systemctl restart flask-codex-chart.service
-sudo docker rm -f codex-react-chart
-sudo docker build -t codex-react-chart .
-sudo docker run -p 8080:80 -d --name codex-react-chart codex-react-chart
-sudo docker ps
