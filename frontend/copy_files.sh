eda65888c652311af2242162ac299e4d5998f0ab
Anil Prajapati <anilprajapati.bs22@gmail.com>
2023-03-30 16:02:42 +0000

    sgnons-commit-message

--- frontend/copy_files.sh
+++ frontend/copy_files.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+# bash copy-files.sh sgnons-commit-message
 rm -r codex-liam-repo/frontend
 rm -r codex-liam-repo/backend
 scp -r presidio@192.168.1.11:/Users/presidio/sgn/metamorf/codex-code/frontend /home/sgnons/codex-liam-repo/
