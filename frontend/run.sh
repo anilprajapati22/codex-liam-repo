d72342a64cad496ba6129eac1252e9dabd3ebb82
Ubuntu <metamorf@metamorf-codex-server.11hh5ngsaqcedbf1ummwqtevqf.zx.internal.cloudapp.net>
2023-04-09 19:46:41 +0000

    server changes

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
-sudo systemctl restart flask-codex
-#sudo rm -r react-frontend/
-sudo docker rm -f codex-react
-sudo docker build -t codex-react /home/metamorf/react-frontend
-sudo docker run -p 80:80 -d --name codex-react codex-react
+# sudo systemctl restart flask-codex
+# sudo rm -r react-frontend/
+sudo docker rm -f codex-react-chart-frontend
+sudo docker build -t codex-react-chart-frontend  /home/metamorf/github/codex-liam-repo/frontend/
+sudo docker run -p 8080:80 -d --name codex-react-chart-frontend codex-react-chart-frontend
 sudo docker ps
 
 #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
